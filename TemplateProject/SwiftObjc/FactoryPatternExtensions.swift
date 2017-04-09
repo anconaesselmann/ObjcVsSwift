2542cc38c02b84df24f96ad61de29c3ecee4ee62
anconaesselmann <axel@anconaesselmann.com>
2017-04-09 16:15:59 -0700

    Updates factory methods

--- TemplateProject/SwiftObjc/FactoryPatternExtensions.swift
+++ TemplateProject/SwiftObjc/FactoryPatternExtensions.swift
@@ -1,30 +1,18 @@
-//  Created by Axel Ancona Esselmann on 10/23/16.
-//  Copyright © 2016 Vida. All rights reserved.
+//  Created by Axel Ancona Esselmann on 4/9/17.
+//  Copyright © 2017 Vida. All rights reserved.
 //
 
 import UIKit
 
-extension Bundle {
-    static var moduleName: String {
-        let bundleKey = kCFBundleNameKey as String
-        return self.application.infoDictionary![bundleKey] as! String
-    }
-    static var application: Bundle {
-        return Bundle.main
-    }
-}
-
 extension String {
     var className: String {
-        return "\(Bundle.moduleName).\(self)"
+        let key = kCFBundleNameKey as String
+        let moduleName = Bundle.main.infoDictionary![key] as!String
+        return "\(moduleName).\(self)"
     }
+    
     var classType: Any.Type? {
-        if let type = NSClassFromString(self.className) {
-            // Swift-class-names have the bundle name prepended
-            return type
-        } else {
-            // Objective-C-class-names don't have the bundle name prepended
-            return NSClassFromString(self)
-        }
+        if let type = NSClassFromString(self.className) { return type }
+        else { return NSClassFromString(self) }
     }
 }
